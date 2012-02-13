260d6c68efa431aedec05d1b26ea59f1afe5f3c9
Perki <pml@defitech.ch>
2012-02-13 17:32:21 +0100

    buggy var

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ port=8123;
 echo "launching gollum on branch:    $branch_name   \n Ctrl+C to quit"
 
 sh -c "sleep 2; open http://localhost:$port" &
-gollum --ref $branch --port $port
+gollum --ref $branch_name --port $port
