fbe1b27d95842f03f1500c87d7861fafd7b9a702
Perki <pm@perki.com>
2019-05-03 17:48:33 +0200

    Switching to yaml

--- source/functional-specifications/index.js
+++ source/functional-specifications/index.js
@@ -1,10 +1,21 @@
 // for loading .coffee files
 require('coffee-script').register();
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+function loadYaml(filename) {
+  try {
+    return yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, filename), 'utf8'));
+  } catch (error) {
+    throw (new Error("while parsing " + path.resolve(__dirname, filename) + '\n' + error.message));
+  };
+}
 
 exports.sections = [
   require('./intro.coffee'),
   require('./terms.coffee'),
-  require('./requirements.coffee'),
+  loadYaml('./requirements.yml'),
 ];
 
 exports.version = '0.0.1';
