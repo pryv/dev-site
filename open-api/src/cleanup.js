9a4f7c2e54022a44e67f98b47dd15bfd6ca41364
Ilia Kebets <kebetsi@gmail.com>
2019-12-10 11:51:15 +0100

    cleanup empty arrays

--- open-api/src/cleanup.js
+++ open-api/src/cleanup.js
@@ -2,7 +2,9 @@ module.exports.removeNulls = function removeNulls(object) {
   const keys = Object.keys(object);
   keys.forEach(k => {
     if (object[k] == null) delete object[k];
+    if (typeof object[k] == 'object' && object[k].length == 0) delete object[k];
     if (typeof object[k] === 'object') removeNulls(object[k]);
+
   });
   return object;
 }
