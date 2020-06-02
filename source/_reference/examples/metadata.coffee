30bd789c722371b05420c3e182b0d56daeb4b44f
Ilia Kebets <kebetsi@gmail.com>
2020-06-02 15:22:47 +0200

    update version to 1.5.7

--- source/_reference/examples/metadata.coffee
+++ source/_reference/examples/metadata.coffee
@@ -2,6 +2,6 @@ timestamp = require("unix-timestamp")
 
 module.exports =
   apiResponse:
-    apiVersion: "1.5.4"
+    apiVersion: "1.5.7"
     serial: "20190428"
     time: timestamp.now()
--- source/_reference/index.js
+++ source/_reference/index.js
@@ -8,7 +8,7 @@ exports.sections = [
   require('./data-structure.coffee')
 ];
 
-exports.version = '1.5.4';
+exports.version = '1.5.7';
 exports.helpers = require('./helpers.coffee');
 
 exports.system = require('./system.coffee');
